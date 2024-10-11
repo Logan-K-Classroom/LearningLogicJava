949fd8426bd3d72fde3dc5a0344525c3232d4a0f
LogEman <thelogankessler@gmail.com>
2024-10-11 02:30:38 +0000

    Worlds best lie detector

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args){
+
+        boolean exampleBool = true;
+        String currentUser = "Jun Brewer";
+
+        double age = 17;
+
+        if(currentUser != "Jun Brewer"){
+            System.out.println("Hello Jun Brewer!");
+        }
+
+        if(age > 51){
+            System.out.println("You are cooked!");
+        }
+
+        System.out.println("Goodbye!");
+        
+    }
+}
