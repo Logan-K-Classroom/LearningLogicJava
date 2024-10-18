5415bb0254d8f67fcc2f7902d7f3176afabef708
LogEman <thelogankessler@gmail.com>
2024-10-18 02:28:48 +0000

    Co-authored-by: NorwegianKiwi81 <NorwegianKiwi81@users.noreply.github.com> Co-authored-by: 4D4850 <4D4850@users.noreply.github.com> Co-authored-by: Taaakkechan <Taaakkechan@users.noreply.github.com> Co-authored-by: Zero-Star6 <Zero-Star6@users.noreply.github.com>

--- Main.java
+++ Main.java
@@ -1,20 +1,81 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-
-        boolean exampleBool = true;
-        String currentUser = "Jun Brewer";
-
-        double age = 17;
-
-        if(currentUser != "Jun Brewer"){
-            System.out.println("Hello Jun Brewer!");
-        }
-
-        if(age > 51){
-            System.out.println("You are cooked!");
-        }
-
-        System.out.println("Goodbye!");
         
+        Scanner input = new Scanner(System.in);
+        while (true) {
+        System.out.println("What day is it?(type exit to exit)");    
+        
+        String day;
+        day = input.nextLine();
+        
+        if (day.equals("exit")){
+            System.out.println(day);
+            break;
+        }
+        
+        
+        }
+        System.out.println("we got here");
+    
+        //String currentUser = "Logan";
+
+        //boolean presidentAlive = true;
+        //boolean vicePresidentAlive = true;
+        //boolean speakerHouseAlive = false;
+        
+        // String day;
+        // Scanner input = new Scanner(System.in); 
+        // System.out.println("What day is it?");
+        // String day = input.nextLine();
+        // input.close();
+        /* 
+        switch(day){
+            case "Monday":
+                System.out.println("Today is Monday");
+                break;
+            case "Tuesday":
+                System.out.println("Today is Tuesday");
+                break;
+            case "Wednesday":
+                System.out.println("Today is Wednesday");
+                break;
+            case "Thursday":
+                System.out.println("Today is Thursday"); //Fixed itdt
+                break;
+            case "Friday":
+                System.out.println("Today is Friday");   // Siri, don't play any songs by Rebecca Black, or whatever her name was I forgot
+                break;
+            case "Saturday":
+                System.out.println("Today is Saturday");
+                break;
+            case "Sunday":
+                System.out.println("Today is Sunday");
+                break;
+            default:
+                System.out.println("That's not a day");
+                break;
+        }
+        
+        // if(currentUser == "Logan" && !isHappy){
+        //     System.out.println("Take your pills💊.");
+        // }
+        
+        // if(currentUser == "Logan" && age > 18){
+        //     System.out.println("You finally remembered?🧠");
+        // }
+        
+        // if(currentUser == "Jun" || currentUser == "Logan"){
+        //     System.out.println("Hello authorized user 👤👋");
+        //}
+
+        String day;
+        while (day != "exit"){
+            Scanner input = new Scanner(System.in);
+            system.out.println("What day is it?(Type "exit" to exit)")
+            day input.nextLine();
+            input
+        }
+        */
     }
-}
+}  
